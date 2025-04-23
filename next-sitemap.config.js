e53e465cebb813af2edcab9b0d341725f29f7971
Bot_Kvv <bkenenbaev30@gmail.com>
2025-04-23 18:05:30 +0500

    sitemap2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://smartassist.kz',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    outDir: './public', // чтобы файл оказался в /public
-  };
+  siteUrl: 'https://your-domain.com', // замени на свой домен
+  generateRobotsTxt: true, // автоматически сгенерирует robots.txt
+  sitemapSize: 7000, // при необходимости разбивает sitemap
+  changefreq: 'weekly',
+  priority: 0.7,
+  trailingSlash: false,
+};
